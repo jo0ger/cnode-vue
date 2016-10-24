276e568e229bf0849e45458f5cef00f1c63fe3e3
zhuzhiyang <zhuzhiyang@91taogu.com>
2016-10-24 13:07:31 +0800

    新增api页面

--- webpack.config.js
+++ webpack.config.js
@@ -4,86 +4,82 @@ var path = require('path'),
 
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/cnode-vue/dist/',
-    filename: 'build.js'
-  },
-  resolveLoader: {
-    root: path.join(__dirname, 'node_modules'),
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.vue$/,
-        loader: 'vue'
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        loader: ExtracTextPlugin.extract({
-          fallbackLoader: "style-loader",
-          loader: "css-loader?sourceMap"
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/cnode-vue/dist/',
+        filename: 'build.js'
+    },
+    resolveLoader: {
+        root: path.join(__dirname, 'node_modules'),
+    },
+    module: {
+        loaders: [{
+            test: /\.vue$/,
+            loader: 'vue'
+        }, {
+            test: /\.js$/,
+            loader: 'babel',
+            exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            loader: ExtracTextPlugin.extract({
+                fallbackLoader: "style-loader",
+                loader: "css-loader?sourceMap"
+            })
+        }, {
+            test: /\.(sass|scss)$/,
+            loader: ExtracTextPlugin.extract({
+                fallbackLoader: "style-loader",
+                loader: "css-loader?sourceMap!sass-loader"
+            })
+        }, {
+            test: /\.(png|jpg|gif|svg)$/,
+            loader: 'file',
+            query: {
+                name: '[name].[ext]?[hash]'
+            }
+        }, {
+            test: /\.(eot|ttf|woff)$/,
+            loader: 'file'
+        }, {
+            test: /\.md$/,
+            loader: "file"
+        }]
+    },
+    plugins: [
+        new ExtracTextPlugin({
+            filename: "style.css",
+            allChunks: true,
+            disable: false
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery"
         })
-      },
-      {
-        test: /\.(sass|scss)$/,
-        loader: ExtracTextPlugin.extract({
-          fallbackLoader: "style-loader",
-          loader: "css-loader?sourceMap!sass-loader"
-        })
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file',
-        query: {
-          name: '[name].[ext]?[hash]'
-        }
-      },
-      {
-        test: /\.(eot|ttf|woff)$/,
-        loader: 'file'
-      }
-    ]
-  },
-  plugins: [
-    new ExtracTextPlugin({
-      filename: "style.css",
-      allChunks: true,
-      disable: false
-    }),
-    new webpack.ProvidePlugin({
-      $: "jquery"
-    })
-  ],
-  resolve: {
-    vue: "vue/dist/vue.js"
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true
-  },
-  devtool: '#eval-source-map'
+    ],
+    resolve: {
+        vue: "vue/dist/vue.js"
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true
+    },
+    devtool: '#eval-source-map'
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    })
-  ])
+    module.exports.devtool = '#source-map'
+        // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ])
 }
